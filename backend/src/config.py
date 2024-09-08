d1401575f58035a665e97f8546f2fb9301d5b9d0
IAINTGOTTIME <vlad-kotov-01-01@mail.ru>
2024-09-08 16:30:56 +0300

    alembic hyeta +

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,2 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+
 PROJECT_NAME = 'HPGolia'
 PROJECT_VERSION = '0.0.0'
