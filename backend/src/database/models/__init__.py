5afe5fbad77545e3f7ba754d2444a7b86a157dfa
memeladon <bingorussian@mail.ru>
2024-09-08 17:11:18 +0300

    deleting models

--- backend/src/database/models/__init__.py
+++ backend/src/database/models/__init__.py
@@ -1,7 +1,1 @@
-from .base import db_connect
-from .dice_roll import DiceRoll
-from .field import Field, FieldType
-from .inventory_item import InventoryItem
-from .item import Item, ItemType
-from .player import Player
-from .player_position import PlayerFieldPosition
+from .base import Base
