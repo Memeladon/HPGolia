489b5ba60e24dc9220639feaf897f217e6883afa
Memeladon <bingorussian@mail.ru>
2024-09-08 17:57:49 +0300

    Alembic test (#28)
    * alembic hyeta
    +
    
    * alembic hyeta 2
    
    * alembic hyeta finalochka
    
    * Db remont connction (#26)
    
    * deleting models
    
    * pochenil connect to db
    
    * docker
    pipfile
    
    * test alembic
    
    ---------
    
    Co-authored-by: IAINTGOTTIME <vlad-kotov-01-01@mail.ru>

--- backend/src/start.py
+++ backend/src/start.py
@@ -5,9 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.config import PROJECT_VERSION, PROJECT_NAME
-from src.database.register import init_db
 from src.routers import users, games
-
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
@@ -34,8 +32,3 @@ def home():
     return "Root page"
 
 
-@app.on_event("startup")
-def on_startup():
-    print("Database initialization...")
-    init_db()
-    print("Initialization completed.")
