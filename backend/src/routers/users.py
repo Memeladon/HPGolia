01ec7b6558906c3081eaaca115fcb5887e790f1c
lardira <danilkefimov@gmail.com>
2024-09-08 22:30:32 +0300

    users api

--- backend/src/routers/users.py
+++ backend/src/routers/users.py
@@ -1,29 +1,23 @@
 from fastapi import APIRouter
 
-from src.database.models import get_db
-from src.database.repositories.player import create_player, update_player
-
-router = APIRouter(prefix="/players", tags=["players"])
+router = APIRouter(tags=['users'], prefix='/users')
 
 
-@router.post("/create_new_one")
-def create_new_player(username: str, age: int, biography: str):
-    db = get_db()
-    player = create_player(username, age, biography, db)
-    return player
+@router.get('/')
+def get_all_user():
+    return []
 
 
-# @router.post("/initialize")
-# def initialize_players():
-#     db = next(db_connect.get_db())
-#     init_players(db)
-#     return {"message": "Players initialized successfully"}
+@router.get('/{id}')
+def get_one_user(id):
+    return {}
 
 
-@router.put("/{player_id}")
-def update_player_info(player_id: int, **kwargs):
-    db = get_db()
-    updated_player = update_player(player_id, db, **kwargs)
-    if updated_player:
-        return {"status": "success", "player": updated_player}
-    return {"status": "error", "message": "Player not found"}
+@router.put('/{id}')
+def update_one_user(id):
+    return {}
+
+
+@router.delete('/{id}')
+def delete_one_user(id):
+    return {"msg": "ok"}
--- backend/src/start.py
+++ backend/src/start.py
@@ -1,11 +1,9 @@
 import os
 
-import psycopg2
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.config import PROJECT_VERSION, PROJECT_NAME
-from src.routers import users, games
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
@@ -23,8 +21,8 @@ app.add_middleware(
 )
 
 # ------------ Routing ------------ #
-app.include_router(users.router)
-app.include_router(games.router)
+# app.include_router(users.router)
+# app.include_router(games.router)
 
 
 @app.get("/")
